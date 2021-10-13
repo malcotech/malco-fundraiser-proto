182fc81c36d0d466ade83c6aa7a60bfab395c00b
Mark Alcoran <m.louie84@gmail.com>
2021-10-12 20:12:16 -0700

    malco-proto-donation-app - config - setup React app.

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    // hot: true,
+    port: 3000,
+    // publicPath: 'http://localhost:3000/dist/'
+    proxy: {
+      '/dist': 'http://localhost:3000'
+    },
+    static: path.join(__dirname, 'public/'),
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
